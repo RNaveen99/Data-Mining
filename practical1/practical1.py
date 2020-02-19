dee9dd68e4ff0e098cf626e2d56c6c9c850d05f1
Naveen Rohilla <naveenrohilla99@gmail.com>
2020-02-19 11:24:45 +0530

    Practical 1

--- practical1/practical1.py
+++ practical1/practical1.py
@@ -1,66 +1,64 @@
 import pandas as pd
-df = pd.read_csv('practical1.txt', sep=' ')
+import numpy as np
+import matplotlib.pyplot as plt
 
-def ageRange(df):
+dataset = pd.read_csv('practical1.txt', sep=' ')
+
+def ageRange(dataset):
     age_range = lambda r : r in range(151)
-    result = df['Age'].apply(age_range)
-    # print(result)
-    # print(df[result])
-    return df[result]
+    result = dataset['Age'].apply(age_range)
+    return result
+    # return dataset[result]
 
-def ageGreaterThanYearsMarried(df):
+def ageGreaterThanYearsMarried(dataset):
     age_greater_than_yearsmarried = lambda x : x['Age'] > x['yearsmarried']
-    result = df[['Age', 'yearsmarried']].apply(age_greater_than_yearsmarried, axis = 1)
-    # print(result)
-    # print(df[result])
-    return df[result]
+    result = dataset[['Age', 'yearsmarried']].apply(age_greater_than_yearsmarried, axis = 1)
+    return result
+    # return dataset[result]
 
-def checkStatus(df):
+def checkStatus(dataset):
     status = lambda x : x == 'married' or x == 'single' or x == 'widowed'
-    result = df['status'].apply(status)
-    # print(result)
-    # print(df[result])
-    return df[result]
+    result = dataset['status'].apply(status)
+    return result
+    # return dataset[result]
 
 
-def checkAgeGroup(df):
+def checkAgeGroup(dataset):
     def age_group(x):
         if (x['Age'] < 18 and x['agegroup'] == 'child'):
             return True
-        elif ((x['Age'] > 18 and x['Age'] < 65) and x['agegroup'] == 'adult'):
+        elif ((x['Age'] >= 18 and x['Age'] < 66) and x['agegroup'] == 'adult'):
             return True
         elif (x['Age'] > 65 and x['agegroup'] == 'elderly'):
             return True
         else:
             return False
-    result = df[['Age', 'agegroup']].apply(age_group, axis = 1)
-    # print(result)
-    # print(df[result])
-    return df[result]
+    result = dataset[['Age', 'agegroup']].apply(age_group, axis = 1)
+    return result
+    # return dataset[result]
 
-# df = ageRange(df)
+a = ageRange(dataset)
+b = ageGreaterThanYearsMarried(dataset)
+c = checkStatus(dataset)
+d = checkAgeGroup(dataset)
 
-# df = ageGreaterThanYearsMarried(df)
+datasetRules = {0 : a, 1 : b, 2 : c, 3 : d}
 
-# df = checkStatus(df)
+X = [True] * len(dataset)
 
-# df = checkAgeGroup(df)
-print('-' * 15, 'Original Data', '-' * 10)
-print(df)
-print('-' * 50)
+rulesBrokenCount = np.empty(len(datasetRules))
 
-print('-' * 15, 'Age in range 0-150', '-' * 10)
-print(ageRange(df))
-print('-' * 50)
+for i in datasetRules:
+    rulesBrokenCount[i] = len(datasetRules[i]) - datasetRules[i].value_counts().at[True]
+    X = X & datasetRules[i]
 
-print('-' * 15, 'Age greater than years married', '-' * 10)
-print(ageGreaterThanYearsMarried(df))
-print('-' * 50)
+for i in range(len(rulesBrokenCount)):
+    print(f'Rule {i + 1} invalidation =  {int(rulesBrokenCount[i])} times')
 
-print('-' * 15, 'Status should be married or single or widowed', '-' * 10)
-print(checkStatus(df))
-print('-' * 50)
+r = np.array(['rule1', 'rule2', 'rule3', 'rule4'])
 
-print('-' * 15, 'Age Group', '-' * 10)
-print(checkAgeGroup(df))
-print('-' * 50)
+plt.bar(r, rulesBrokenCount)
+plt.xlabel('Rules')
+plt.ylabel('Count of Rules Broken')
+# dataset.boxplot()
+plt.show()
